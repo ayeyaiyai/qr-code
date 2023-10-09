0dee57daa2a8e33ac36dc20d8363d4b095fae8f0
ayeyaiyai <connorgilbert77@gmail.com>
2023-10-09 14:49:35 -0400

    Add QRCode image

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -1,9 +1,12 @@
 import '../styles/QRCode.css';
+import QRCodeImage from '../images/image-qr-code.png';
 
 function QRCode() {
     return(
         <div className='qr-code-body'>
-            Hello!
+            <div className='qr-image'>
+                <img src={QRCodeImage} alt='QR Code' className='qr-code-image' />
+            </div>
         </div>
     )
 }
