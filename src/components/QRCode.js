56a54b1b03341e7f20104dae83634bca3365bcf4
ayeyaiyai <connorgilbert77@gmail.com>
2023-10-09 14:51:22 -0400

    Add all html for QRCode.js

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -7,6 +7,10 @@ function QRCode() {
             <div className='qr-image'>
                 <img src={QRCodeImage} alt='QR Code' className='qr-code-image' />
             </div>
+            <div className='qr-body'>
+                <div className='qr-title'>Improve your front-end skills by building projects</div>
+                <div className='qr-description'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+            </div>
         </div>
     )
 }
